456beb37984d604a4d6f04c15898c357aa02353a
Ed Hall <edward.hall.144@gmail.com>
2022-03-09 08:24:03 +0000

    Create sendmail.php

---
+++ sendmail.php
@@ -0,0 +1,26 @@
+<?php
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    require_once(dirname(__DIR__) . '/vendor/autoload.php');
+
+    (PHP_SAPI !== 'cli' || isset($_SERVER['HTTP_USER_AGENT'])) && die('Must run via CLI');
+
+    if(isset($argc)) {
+
+    }
+    else {
+        die("argc is disabled\n");
+    }
+
+    $smtp = [
+
+    ];
+
+     //Set basic details
+     $mail = new PHPMailer();
+     $mail->isHTML(true);
+     $mail->Subject = $subject;
+     $mail->Body = $template;
