2514bf5358ecd74712eca8f42a7df42d6f5e152a
Ed Hall <edward.hall.144@gmail.com>
2022-03-09 10:39:00 +0000

    Create smtp_credentials.php

---
+++ smtp_credentials.php
@@ -0,0 +1,17 @@
+<?php
+
+    $smtpCredentials = [
+        "host" => "",
+        "username" => "",
+        "password" => "",
+        "port" => 465
+    ];
+
+    $addresses = [
+        "from_address" => "",
+        "from_friendly" => "",
+        "reply_address" => "",
+        "reply_friendly" => ""
+    ];
+
+?>
